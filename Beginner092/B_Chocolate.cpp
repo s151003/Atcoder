8bedf2025f1f8327472ddfe88572f792510ee4a1
s151003 <s151003@kashiwanoha.ed.jp>
2018-03-26 15:50:49 +0000

    途中

---
+++ Beginner092/B_Chocolate.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main(void){
+	int i;
+	int N,D,X,A[N];
+	int cnt;
+
+	cin>>N>>D>>X;
+	for(i=1;i<=N;i++){
+		cnt+=1;
+		cin>>A[i];
+	}
+	return 0;
+}
