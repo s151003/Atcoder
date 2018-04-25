83aa248ae6d8c06c0f814e921e58fe3d394aa264
s151003 <s151003@kashiwanoha.ed.jp>
2018-04-26 01:19:47 +0900

    削除
    動かないし意味が分からなくなった
    考えなおす

--- Beginner095/C_HalfAndHalf.py
+++ Beginner095/C_HalfAndHalf.py
@@ -1,18 +1,6 @@
 A, B, C, X, Y = map(int, input().split())
 
-if A <= C or B <= C:
-  if X == min(X,Y):
-    val = X * C * 2
-    val = val + ((Y-X)*B)
-  else:
-    val = Y * C * 2
-    val = val + ((X-Y)*A)
-
-val_s = (A*X) + (B*Y)
-
-if val > val_s:
-  ans = val_s
+if (A+B) > (C*2):
+  
 else:
-  ans = val
-
-print(ans)
+  ans = A * X + B * Y
