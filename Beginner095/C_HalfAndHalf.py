9dbfead960ad2b6f14f67c53dddefbfe1fd2ed78
s151003 <s151003@kashiwanoha.ed.jp>
2018-04-26 00:46:15 +0900

    debug削除

--- Beginner095/C_HalfAndHalf.py
+++ Beginner095/C_HalfAndHalf.py
@@ -4,15 +4,11 @@ if A <= C or B <= C:
   if X == min(X,Y):
     val = X * C * 2
     val = val + ((Y-X)*B)
-    print("X")
   else:
     val = Y * C * 2
     val = val + ((X-Y)*A)
-    print("Y")
-  print(val)
 
 val_s = (A*X) + (B*Y)
-print(val_s)
 
 if val > val_s:
   ans = val_s
